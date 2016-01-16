e0d0ba88b388cc93f97ecb2fc7647084a3449b55
Roger Tan <roger.tan@airbnb.com>
2016-01-16 15:59:41 -0800

    Working Calculator

---
+++ tips/tips/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by Roger Tan on 1/16/16.
+//  Copyright © 2016 Roger Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00";
+        totalLabel.text = "$0.00";
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        
+        var billAmount = (billField.text! as NSString).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+}
+
